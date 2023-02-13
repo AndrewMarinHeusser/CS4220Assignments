174adb06849e0a2e1b781dc3806dc734d223e515
AndrewMarinHeusser <heusser.andrew@gmail.com>
2023-02-13 00:51:52 -0800

    Homework1 Complete

--- Week3/Homework1.js
+++ Week3/Homework1.js
@@ -22,10 +22,9 @@ function sumEvenOdd(num){
     let numCombine = 0;
     let numArray = Array.from(String(num),Number);
 
-    for(let i = 0; i < numArray; i++){
-        console.log("it worked at all");
+    for(let i = 0; i < numArray.length; i++){
         numCombine = numCombine + numArray[i];
-        console.log(numArray[i]);
+        //console.log(numCombine);
     }
 
     if(numCombine % 2 == 0){
@@ -38,4 +37,91 @@ function sumEvenOdd(num){
 }
 
 console.log(sumEvenOdd(11));
-console.log(sumEvenOdd(11));
+console.log(sumEvenOdd(12345));
+
+
+//Number 3
+
+function countNestedWord(longString,subString){
+    let currentCount = 0;
+
+    let caseFixString = longString.toUpperCase();
+    let caseFixSub = subString.toUpperCase();
+
+    for (let i = 0; i < caseFixString.length; i++){
+        if (caseFixString.substr(i, caseFixSub.length) === caseFixSub) {
+            currentCount++;
+        }
+    }
+
+    return "output: " + currentCount;
+}
+
+console.log(countNestedWord('nodejavascriptnodevscodejsnodejavascriptvscodenopenonenode', 'node'));
+
+ console.log(countNestedWord('There are many differences between Java and JavaScript','java'));
+
+  //Number 4
+
+  function cleanPhrase(input){
+     let cleanedInput = ""; 
+     for(let i = 0; i < input.length; i++){
+        if ((isNaN(input[i]))){
+            cleanedInput += input[i];
+        }
+     }
+    return cleanedInput
+  }
+
+  console.log(cleanPhrase('Th7e qu1ick br8own fo0x jum1ps over 22the la9zy d3og5.'))
+
+  //Number 5
+
+  function reverseAtWord(inputArray,inputWord){
+    let current = inputArray.indexOf(inputWord);
+    if(current === -1) return inputArray;
+    return inputArray.slice(0, current).concat(inputArray.slice(current).reverse());
+  }
+
+  console.log(reverseAtWord(['alpha', 'beta', 'gamma', 'delta', 'epsilon'], 'gamma'))
+
+  //Number 6
+  function ascendingArray(ascend){
+    
+    let originalLength = ascend.length;
+    let currentSmallestPos;
+    let sortedAscend = [];
+
+    for(let i = 0; i < originalLength; i++){
+        let currentSmallest = Number.MAX_SAFE_INTEGER;
+        for(let j = 0; j < ascend.length; j++){
+            if(ascend[j].length <= currentSmallest){
+                currentSmallest = ascend[j].length;
+                currentSmallestPos = j;
+            }
+        }
+    sortedAscend.push(ascend[currentSmallestPos]);
+    ascend.splice(currentSmallestPos,1);
+    }
+
+    return sortedAscend;
+  }
+
+  console.log(ascendingArray(['march', 'may', 'august', 'june']));
+
+
+  //Number 7
+
+  function letterMap(letterArray){
+    let map = {}
+    let sortedLetterArray = letterArray.sort(function (a, b) {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+    });
+
+    sortedLetterArray.forEach(function(character){
+        map[character.toLowerCase()] = character.toUpperCase();
+    });
+    return map;
+  }
+
+  console.log(letterMap(['Z', 'a', 'b', 'y', 'x', 'c']));
