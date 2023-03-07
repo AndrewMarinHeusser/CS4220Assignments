d6f77c95a7e77d38bb7da315c83c1f0978dd8aac
AndrewMarinHeusser <Heusser.andrew@gmail.com>
2023-03-06 21:24:32 -0800

    week 7 and hw

--- Week6/request-template.js
+++ Week6/request-template.js
@@ -1,5 +1,7 @@
 // require in your prefered module/library to make http requests (superagent, axios, etc)
 
+const superagent = require('superagent');
+
 const websites = {
     dummyjson: {
         name: 'Dummy JSON',
@@ -20,9 +22,39 @@ const websites = {
 };
 
 // // # 2 - Write the requestPromise() function which uses your http request module
-function requestPromise(url,callee){
-    
-}
+const requestPromise = (url, callee) => {
+    return new Promise((resolve,reject) => {
+        if(!url|| !url.trim()){
+            reject({
+                url:url,
+                statusCode: null,
+                callee:callee
+            });
+        }
+        else{
+            const start = Date.now();
+            superagent.get(url).end((error, response) =>{
+                const end = Date.now();
+                if(error || !response.ok){
+                    reject({
+                        url: url,
+                        statusCode: response ? response.status : null,
+                        callee: callee
+                    });
+                }
+                else{
+                    resolve({
+                        url: url,
+                        duration: end - start,
+                        statusCode: response.status,
+                        contentType: response.header['content-type'],
+                        callee: callee
+                    });
+                }
+            });
+        }
+    });
+};
 
 // // # 3 - Invoke the requestPromise() using then/catch for each websites object above
 
--- Week6/superagent-example/superagent-request.js
+++ Week6/superagent-example/superagent-request.js
@@ -1,13 +1,42 @@
 /*
+    // Downloading from Canvas with a package.json
+    1. run: npm install
+    2. run the demo: node superagent-request.js
 
-*/
 
-const cdnjs = 'https://api.cdnjs.com/libraries';
+    // Starting from a brand new Node.js project with NO package.json
+    1. run: npm init
+    2. answer the question prompts on your terminal
+    3. verify: package.json was created
+    4. run: npm install --save <npm module you need>
+ */
+const superagent = require('superagent');
+
+const cdnjs = 'https://api.cdnjs.com/libraries?limit=10';
 
 // use superagent with callbacks
-const superagentCallbacks = (url) => {};
-// superagentCallbacks(cdnjs);
+const superagentCallbacks = (url) => {
+    superagent.get(url).end((error, res) => {
+        if (!error) {
+            console.log(res.headers);
+            console.log(res.statusCode);
+            console.log(res.body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+superagentCallbacks(cdnjs);
 
 // use superagent with async/await
-const superagentAsyncAwait = async (url) => {};
+const superagentAsyncAwait = async (url) => {
+    try {
+        const res = await superagent.get(url);
+        console.log(res.headers);
+        console.log(res.statusCode);
+        console.log(res.body);
+    } catch (error) {
+        console.log(error);
+    }
+};
 superagentAsyncAwait(cdnjs);
